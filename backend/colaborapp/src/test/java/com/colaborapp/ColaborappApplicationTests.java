202fc02dd9cbed446d09773455001a286c0a2124
Héctor Cortez <updown.input@gmail.com>
2024-02-13 12:54:01 -0300

    initial project structure

---
+++ backend/colaborapp/src/test/java/com/colaborapp/ColaborappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colaborapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColaborappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
