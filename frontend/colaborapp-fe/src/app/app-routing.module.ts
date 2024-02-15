e4af8000313064b64a82835e741643920cfd12eb
analia-spicona <analia_spicona@hotmail.com>
2024-02-15 17:19:27 -0300

    Formularios login y register (sin diseño)

--- frontend/colaborapp-fe/src/app/app-routing.module.ts
+++ frontend/colaborapp-fe/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/users/login/login.component';
+import { RegisterComponent } from './components/users/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent },
+  //{path: 'dashboard', component:DashboardComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'register', component: RegisterComponent}
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/colaborapp-fe/src/app/app.module.ts
+++ frontend/colaborapp-fe/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/users/register/register.component';
+import { LoginComponent } from './components/users/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
