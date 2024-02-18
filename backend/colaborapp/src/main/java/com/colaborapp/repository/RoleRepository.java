bcb955e0f98aa11088862a126bcb8cf39447a620
Héctor Cortez <updown.input@gmail.com>
2024-02-17 22:47:58 -0300

    Repository for Role created.

---
+++ backend/colaborapp/src/main/java/com/colaborapp/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.colaborapp.repository;
+
+import com.colaborapp.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
