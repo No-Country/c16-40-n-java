488f6ce84ff91bb3c0c9557bbb3d61ac749b3ff2
Héctor Cortez <updown.input@gmail.com>
2024-02-17 22:50:49 -0300

    Method for registration and find user by email created and implemented.

--- backend/colaborapp/src/main/java/com/colaborapp/service/UserService.java
+++ backend/colaborapp/src/main/java/com/colaborapp/service/UserService.java
@@ -1,4 +1,10 @@
 package com.colaborapp.service;
 
+import com.colaborapp.dto.UserRequestDTO;
+import com.colaborapp.model.User;
+
 public interface UserService {
+    void userRegistration(UserRequestDTO registrationRequest);
+
+    User getUserByEmailFromDatabase(String email);
 }
