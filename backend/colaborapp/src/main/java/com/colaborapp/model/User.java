3e74973f356d4c1e07a8e5d5049f07eb21682e22
Héctor Cortez <updown.input@gmail.com>
2024-02-17 22:43:13 -0300

    Builder pattern hand made with logic inside each method. Phone number attribute added.

--- backend/colaborapp/src/main/java/com/colaborapp/model/User.java
+++ backend/colaborapp/src/main/java/com/colaborapp/model/User.java
@@ -2,14 +2,14 @@ package com.colaborapp.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.Objects;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 @Getter
 public class User {
     @Id
@@ -21,9 +21,134 @@ public class User {
     private String lastName;
     @Column(nullable = false, unique = true)
     private String email;
+    @Column(name = "PHONE_NUMBER")
+    private String phoneNumber;
     @Column(nullable = false)
     private String password;
     @JoinColumn(name = "ROLE_ID")
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
     private Role role;
+    private boolean enable;
+
+    public void setName(String name) {
+        if (Objects.nonNull(name) && !name.trim().isEmpty()) {
+            this.name = name;
+        }
+    }
+
+    public void setLastName(String lastName) {
+        if (Objects.nonNull(lastName) && !lastName.trim().isEmpty()) {
+            this.lastName = lastName;
+        }
+    }
+
+    public void setEmail(String email) {
+        if (Objects.nonNull(email) && !email.trim().isEmpty()) {
+            this.email = email;
+        }
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        if (Objects.nonNull(phoneNumber) && !phoneNumber.trim().isEmpty()) {
+            this.phoneNumber = phoneNumber;
+        }
+    }
+
+    public void setPassword(String password) {
+        if (Objects.nonNull(password) && !password.trim().isEmpty()) {
+            this.password = password;
+        }
+    }
+
+    public void setRole(Role role) {
+        if (Objects.nonNull(role)) {
+            this.role = role;
+        }
+    }
+
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
+
+    public static UserBuilder builder() {
+        return new UserBuilder();
+    }
+
+    public static class UserBuilder {
+        private Long id;
+        private String name;
+        private String lastName;
+        private String email;
+        private String phoneNumber;
+        private String password;
+        private Role role;
+        private boolean enable;
+
+        public UserBuilder id(Long id) {
+            if (Objects.nonNull(id)) {
+                this.id = id;
+            }
+            return this;
+        }
+
+        public UserBuilder name(String name) {
+            if (Objects.nonNull(name) && !name.trim().isEmpty()) {
+                this.name = name;
+            }
+            return this;
+        }
+
+        public UserBuilder lastName(String lastName) {
+            if (Objects.nonNull(lastName) && !lastName.trim().isEmpty()) {
+                this.lastName = lastName;
+            }
+            return this;
+        }
+
+        public UserBuilder email(String email) {
+            if (Objects.nonNull(email) && !email.trim().isEmpty()) {
+                this.email = email;
+            }
+            return this;
+        }
+
+        public UserBuilder phoneNumber(String phoneNumber) {
+            if (Objects.nonNull(phoneNumber) && !phoneNumber.trim().isEmpty()) {
+                this.phoneNumber = phoneNumber;
+            }
+            return this;
+        }
+
+        public UserBuilder password(String password) {
+            if (Objects.nonNull(password) && !password.trim().isEmpty()) {
+                this.password = password;
+            }
+            return this;
+        }
+
+        public UserBuilder role(Role role) {
+            if (Objects.nonNull(role)) {
+                this.role = role;
+            }
+            return this;
+        }
+
+        public UserBuilder enable(boolean enable) {
+            this.enable = enable;
+            return this;
+        }
+
+        public User build() {
+            User user = new User();
+            user.id = this.id;
+            user.name = this.name;
+            user.lastName = this.lastName;
+            user.email = this.email;
+            user.enable = this.enable;
+            user.phoneNumber = this.phoneNumber;
+            user.role = this.role;
+            user.password = this.password;
+            return user;
+        }
+    }
 }
