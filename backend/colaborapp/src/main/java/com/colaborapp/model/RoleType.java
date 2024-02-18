d18b6afc2d79a771296f0c34f8291161dd9509f9
Héctor Cortez <updown.input@gmail.com>
2024-02-17 22:44:39 -0300

    Role prefix added and a method to get the full role name with the prefix.

--- backend/colaborapp/src/main/java/com/colaborapp/model/RoleType.java
+++ backend/colaborapp/src/main/java/com/colaborapp/model/RoleType.java
@@ -1,5 +1,11 @@
 package com.colaborapp.model;
 
 public enum RoleType {
-    ADMIN, USER
+    ADMIN, USER;
+
+    private static final String PREFIX = "ROLE_";
+
+    public String getFullRoleName() {
+        return PREFIX + this.name();
+    }
 }
