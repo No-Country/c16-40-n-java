bcca7dcc8dd69a44027b9b9fe55f5c1a2d528bdd
Héctor Cortez <updown.input@gmail.com>
2024-02-17 22:45:44 -0300

    Method to map to User entity from DTO added.

--- backend/colaborapp/src/main/java/com/colaborapp/model/mapper/UserMapperImpl.java
+++ backend/colaborapp/src/main/java/com/colaborapp/model/mapper/UserMapperImpl.java
@@ -1,7 +1,18 @@
 package com.colaborapp.model.mapper;
 
+import com.colaborapp.dto.UserRequestDTO;
+import com.colaborapp.model.User;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserMapperImpl implements UserMapper {
+    @Override
+    public User toEntity(UserRequestDTO dto) {
+        return User.builder()
+                .name(dto.name())
+                .lastName(dto.lastName())
+                .phoneNumber(dto.phoneNumber())
+                .email(dto.email())
+                .build();
+    }
 }
