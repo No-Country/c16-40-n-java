075c2609d7304be746216fc5300c519a22c977c4
fernandobouchet <fernandobouchet@gmail.com>
2024-02-23 20:29:03 -0300

    feat: add 'HomeContainer' component

--- frontend/components/home/HomeContainer.tsx
+++ frontend/components/home/HomeContainer.tsx
@@ -1,10 +1,24 @@
 import Navbar from '@/components/navbar/Navbar';
+import { Button } from '@/components/ui/button';
 
 const HomeContainer = () => {
   return (
-    <main>
+    <>
       <Navbar />
-    </main>
+      <main className="h-full w-full">
+        <div className="bg-[url('/home_image.png')] bg-center bg-cover pt-5 h-[calc(100dvh-10%)]">
+          <section className="flex flex-col items-center justify-center h-full gap-20 lg:gap-40">
+            <p className="text-primary-foreground font-semibold text-center text-4xl lg:text-6xl">
+              Tu <span className="text-primary">apoyo</span> hace{' '}
+              <span className="text-primary">lo imposible.</span>
+            </p>
+            <Button className="font-bold w-96 h-12 rounded-full lg:text-2xl">
+              Comienza tu proyecto!
+            </Button>
+          </section>
+        </div>
+      </main>
+    </>
   );
 };
 export default HomeContainer;
