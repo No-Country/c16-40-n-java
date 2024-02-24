29cc9684e343acce26b184215e8e4c188bcfb3e6
fernandobouchet <fernandobouchet@gmail.com>
2024-02-23 23:43:10 -0300

    feat: add link to dinamic route to 'projectCard'

--- frontend/components/projectCard.tsx
+++ frontend/components/projectCard.tsx
@@ -8,6 +8,7 @@ import {
 } from '@/components/ui/card';
 import Image from 'next/image';
 import { Progress } from '@/components/ui/progress';
+import Link from 'next/link';
 
 interface Props {
   data: {
@@ -22,18 +23,20 @@ interface Props {
 
 const ProjectCard = ({ data }: Props) => {
   return (
-    <Card className="w-96 bg-white text-secondary-foreground cursor-pointer">
-      <CardHeader className="text-lg">
-        <CardTitle className="text-lg">{data.title}</CardTitle>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <Image src={data.image} alt={''} width={350} height={10} />
-        <CardDescription>{data.description}</CardDescription>
-      </CardContent>
-      <CardFooter>
-        <Progress value={data.progress} />
-      </CardFooter>
-    </Card>
+    <Link href={`/project/${data.id}`}>
+      <Card className="w-96 bg-white text-secondary-foreground cursor-pointer">
+        <CardHeader className="text-lg">
+          <CardTitle className="text-lg">{data.title}</CardTitle>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <Image src={data.image} alt={''} width={350} height={10} />
+          <CardDescription>{data.description}</CardDescription>
+        </CardContent>
+        <CardFooter>
+          <Progress value={data.progress} />
+        </CardFooter>
+      </Card>
+    </Link>
   );
 };
 export default ProjectCard;
