a618bb056065ea70a5e19da808355a37c8de5880
Héctor Cortez <updown.input@gmail.com>
2024-02-27 12:52:34 -0300

    Endpoint to list all categories.

--- backend/colaborapp/src/main/java/com/colaborapp/controller/CategoryController.java
+++ backend/colaborapp/src/main/java/com/colaborapp/controller/CategoryController.java
@@ -1,7 +1,24 @@
 package com.colaborapp.controller;
 
+import com.colaborapp.dto.CategoryResponseDTO;
+import com.colaborapp.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping(value = "/api/categories")
+@RequiredArgsConstructor
 public class CategoryController {
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponseDTO>> listCategories() {
+        return ResponseEntity.status(HttpStatus.OK).body(categoryService.listAllCategories());
+    }
 }
