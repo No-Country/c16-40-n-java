501653f953faa2051528e3869a979c2af7991055
Héctor Cortez <updown.input@gmail.com>
2024-02-27 12:55:29 -0300

    Method to get a Status (enum) from a set of values.

--- backend/colaborapp/src/main/java/com/colaborapp/model/Status.java
+++ backend/colaborapp/src/main/java/com/colaborapp/model/Status.java
@@ -1,5 +1,20 @@
 package com.colaborapp.model;
 
+import java.util.InputMismatchException;
+import java.util.Set;
+
 public enum Status {
-    ACTIVE, FINALIZED, INREVIEW, PENDING, DELETED
+    ACTIVE, FINALIZED, IN_REVIEW, PENDING, DELETED;
+
+    private static final Set<String> states = Set.of(
+            "ACTIVE", "FINALIZED", "IN_REVIEW", "PENDING", "DELETED"
+    );
+
+    public static Status getStatusFromString(String value) {
+        String statusType = value.toUpperCase();
+        if (!states.contains(statusType)) {
+            throw new InputMismatchException("Unknown Status Type: %s".formatted(value));
+        }
+        return Status.valueOf(statusType);
+    }
 }
