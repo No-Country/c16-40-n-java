3241580eaa371262356e1aa97434a7bdf056459f
Héctor Cortez <updown.input@gmail.com>
2024-02-27 12:54:25 -0300

    Get category by type and list all categories.

--- backend/colaborapp/src/main/java/com/colaborapp/service/CategoryService.java
+++ backend/colaborapp/src/main/java/com/colaborapp/service/CategoryService.java
@@ -1,4 +1,13 @@
 package com.colaborapp.service;
 
+import com.colaborapp.dto.CategoryResponseDTO;
+import com.colaborapp.model.Category;
+import com.colaborapp.model.CategoryType;
+
+import java.util.List;
+
 public interface CategoryService {
+    Category getCategoryByType(CategoryType type);
+
+    List<CategoryResponseDTO> listAllCategories();
 }
