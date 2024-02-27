256932f9322bc5aff5aa72e735f20d8fc5da564b
Héctor Cortez <updown.input@gmail.com>
2024-02-27 12:31:35 -0300

    Some important fixes in how to handle creation, modification and deletion on a project object.

--- backend/colaborapp/src/main/java/com/colaborapp/service/ProjectService.java
+++ backend/colaborapp/src/main/java/com/colaborapp/service/ProjectService.java
@@ -2,17 +2,20 @@ package com.colaborapp.service;
 
 import com.colaborapp.dto.ProjectRequestDTO;
 import com.colaborapp.dto.ProjectResponseDTO;
+import com.colaborapp.model.Project;
 
 import java.util.List;
 
 public interface ProjectService {
-    ProjectResponseDTO getProjectById(Long projectId);
+    ProjectResponseDTO createProject(ProjectRequestDTO request);
+
+    ProjectResponseDTO updateProject(Long id, ProjectRequestDTO updateRequest);
+
+    ProjectResponseDTO getProjectById(Long id);
 
     List<ProjectResponseDTO> getAllProjects();
 
-    ProjectResponseDTO updateProject(String creatorEmail, Long projectId, ProjectRequestDTO projectRequestDTO);
+    Project getProjectEntityById(Long id);
 
-    void deleteProject(Long projectId);
-
-    ProjectResponseDTO createProject(String creatorEmail, ProjectRequestDTO projectRequestDTO);
+    void deleteProject(Long id);
 }
