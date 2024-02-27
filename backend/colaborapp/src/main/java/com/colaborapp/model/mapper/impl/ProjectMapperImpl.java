76e22a6350d34baed3415aca564d7d4121a33a38
Héctor Cortez <updown.input@gmail.com>
2024-02-27 12:28:11 -0300

    Name convention for mapping operations and deleted non-necessary methods.

--- backend/colaborapp/src/main/java/com/colaborapp/model/mapper/impl/ProjectMapperImpl.java
+++ backend/colaborapp/src/main/java/com/colaborapp/model/mapper/impl/ProjectMapperImpl.java
@@ -2,71 +2,49 @@ package com.colaborapp.model.mapper.impl;
 
 import com.colaborapp.dto.ProjectRequestDTO;
 import com.colaborapp.dto.ProjectResponseDTO;
+import com.colaborapp.dto.UserResponseDTO;
 import com.colaborapp.model.Project;
-import com.colaborapp.model.Status;
+import com.colaborapp.model.User;
 import com.colaborapp.model.mapper.ProjectMapper;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
-
-import java.time.LocalDate;
 
 
 @Component
 public class ProjectMapperImpl implements ProjectMapper {
 
     @Override
-    public Project toProjectEntity(ProjectRequestDTO projectRequestDTO) {
-        Assert.notNull(projectRequestDTO, "ProjectRequestDTO object must not be null.");
-
-        Status status = projectRequestDTO.status() != null ? Status.valueOf(projectRequestDTO.status()) : Status.PENDING;
-        Double currentAmount = projectRequestDTO.current_amount() != null ? projectRequestDTO.current_amount() : 0.0;
-        LocalDate startDate = projectRequestDTO.startDate() != null ? projectRequestDTO.startDate() : LocalDate.now();
-
+    public Project toEntity(ProjectRequestDTO dto) {
+        Assert.notNull(dto, "ProjectRequestDTO object must not be null.");
         return Project.builder()
-                .title(projectRequestDTO.title())
-                .status(status)
-                .startDate(startDate)
-                .currentAmount(currentAmount)
-                .description(projectRequestDTO.description())
-                .image(projectRequestDTO.image())
-                .goalAmount(projectRequestDTO.goalAmount())
-                .endDate(projectRequestDTO.endDate())
+                .title(dto.title())
+                .description(dto.description())
+                .image(dto.image())
+                .goalAmount(dto.goalAmount())
                 .build();
     }
 
     @Override
-    public ProjectResponseDTO toProjectResponseDto(Project entity) {
+    public ProjectResponseDTO toDTO(Project entity) {
         Assert.notNull(entity, "Entity object must not be null.");
-
+        User creator = entity.getCreator();
         return ProjectResponseDTO.builder()
                 .id(entity.getId())
-                .creator(entity.getCreator().getEmail())
-                .category(String.valueOf(entity.getCategory().getName()))
+                .creator(UserResponseDTO.builder()
+                        .id(creator.getId())
+                        .name(creator.getName())
+                        .lastName(creator.getLastName())
+                        .email(creator.getEmail())
+                        .build())
+                .category(entity.getCategory().getType())
                 .title(entity.getTitle())
                 .description(entity.getDescription())
                 .image(entity.getImage())
-                .status(entity.getStatus().name())
+                .status(entity.getStatus())
                 .goalAmount(entity.getGoalAmount())
                 .currentAmount(entity.getCurrentAmount())
                 .startDate(entity.getStartDate())
                 .endDate(entity.getEndDate())
-                .build();
-    }
-    @Override
-    public Project toProjectEntityForUpdate(Project existingProject, ProjectRequestDTO projectRequestDTO) {
-        Assert.notNull(existingProject, "ExistingProject object must not be null.");
-        Assert.notNull(projectRequestDTO, "ProjectRequestDTO object must not be null.");
-
-        return Project.builder()
-                .id(existingProject.getId())
-                .status(Status.valueOf(projectRequestDTO.status()))
-                .title(projectRequestDTO.title())
-                .description(projectRequestDTO.description())
-                .image(projectRequestDTO.image())
-                .goalAmount(projectRequestDTO.goalAmount())
-                .currentAmount(projectRequestDTO.current_amount())
-                .startDate(existingProject.getStartDate())
-                .endDate(projectRequestDTO.endDate())
                 .build();
     }
 }
