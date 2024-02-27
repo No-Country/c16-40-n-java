c57b30de7393a38b3f8061b8c547836ba6b0ee16
Héctor Cortez <updown.input@gmail.com>
2024-02-27 12:47:20 -0300

    Some important fixes for user authentication and the possibility to get the current username (logged user) from the context.

--- backend/colaborapp/src/main/java/com/colaborapp/service/impl/AuthServiceImpl.java
+++ backend/colaborapp/src/main/java/com/colaborapp/service/impl/AuthServiceImpl.java
@@ -3,14 +3,14 @@ package com.colaborapp.service.impl;
 import com.colaborapp.auth.JwtService;
 import com.colaborapp.dto.AuthRequestDTO;
 import com.colaborapp.dto.AuthResponseDTO;
+import com.colaborapp.model.exception.AccountDisabledException;
 import com.colaborapp.service.AuthService;
 import com.colaborapp.service.UserService;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -25,6 +25,9 @@ public class AuthServiceImpl implements AuthService {
     @Override
     public AuthResponseDTO authentication(AuthRequestDTO request) {
         var user = userService.getUserByEmailFromDatabase(request.email());
+        if (!user.isEnable()) {
+            throw new AccountDisabledException("This account is not longer available.");
+        }
         // creates a user to authenticate
         authManager.authenticate(new UsernamePasswordAuthenticationToken(request.email(), request.password()));
         UserDetails userDetails = User.builder() // build a UserDetails (spring security class)
@@ -37,11 +40,12 @@ public class AuthServiceImpl implements AuthService {
                 .token(jwtService.generateToken(userDetails))
                 .build();
     }
+
     @Override
-    public String getCurrentUserFromToken(String token) {
-        String formattedToken  = extractToken(token);
-        return jwtService.extractUsername(formattedToken);
+    public String getAuthenticatedUsername() {
+        return SecurityContextHolder.getContext().getAuthentication().getName();
     }
+
     private String extractToken(String token) {
         if (token != null) {
             return token.substring(7);
