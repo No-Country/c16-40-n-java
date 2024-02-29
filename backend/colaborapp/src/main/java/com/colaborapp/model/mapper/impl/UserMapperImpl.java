18f5c436d3251218d69a8d60e95f41d4516c2e30
Héctor Cortez <updown.input@gmail.com>
2024-02-29 00:25:12 -0300

    User entity to User DTO method added.

--- backend/colaborapp/src/main/java/com/colaborapp/model/mapper/impl/UserMapperImpl.java
+++ backend/colaborapp/src/main/java/com/colaborapp/model/mapper/impl/UserMapperImpl.java
@@ -1,12 +1,21 @@
 package com.colaborapp.model.mapper.impl;
 
+import com.colaborapp.dto.UserProjectsDTO;
 import com.colaborapp.dto.UserRequestDTO;
+import com.colaborapp.dto.UserResponseDTO;
 import com.colaborapp.model.User;
+import com.colaborapp.model.mapper.ProjectMapper;
 import com.colaborapp.model.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.stream.Collectors;
+
 @Component
+@RequiredArgsConstructor
 public class UserMapperImpl implements UserMapper {
+    private final ProjectMapper projectMapper;
+
     @Override
     public User toEntity(UserRequestDTO dto) {
         return User.builder()
@@ -14,6 +23,27 @@ public class UserMapperImpl implements UserMapper {
                 .lastName(dto.lastName())
                 .phoneNumber(dto.phoneNumber())
                 .email(dto.email())
+                .build();
+    }
+
+    @Override
+    public UserResponseDTO toDTO(User entity) {
+        return UserResponseDTO.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .lastName(entity.getLastName())
+                .email(entity.getEmail())
+                .phoneNumber(entity.getPhoneNumber())
+                .projects(UserProjectsDTO.builder()
+                        .asOwner(entity.getProjects()
+                                .stream()
+                                .map(projectMapper::toBasicDTO)
+                                .collect(Collectors.toSet()))
+                        .asVolunteer(entity.getVolunteeringList()
+                                .stream()
+                                .map(volunteering -> projectMapper.toDTO(volunteering.getProject()))
+                                .collect(Collectors.toSet()))
+                        .build())
                 .build();
     }
 }
